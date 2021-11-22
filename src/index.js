899f38cf7b0ec2bd6080a753c92693f7e9da9027
Corentyn <corentyn.masset@gmail.com>
2021-11-22 15:31:05 +0100

    My first commit on this repo

--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import CompoImg from '../src/components/CompoImg'
+import MEF from '../src/components/MEF'
+import Typical from 'react-typical';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+class App extends React.Component{
+  render(){
+    return(
+      <>
+      
+      <div className="App" style={{color: 'white', textAlign:'center',margin:'50px',fontFamily:'PT Sans'}}>
+      <CompoImg/>
+        <h1>Hi, I'm Corentyn.</h1>
+        <p> I'm a{' '}
+
+          <Typical 
+            loop={Infinity}
+            wrapper="b"
+            steps = {[
+              'Student 📚',
+              1000,
+              'Web Developper 💻',
+              1000,
+            ]}          
+          
+          
+          
+          />
+
+        </p>
+      </div>
+      
+      
+      </>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
+
