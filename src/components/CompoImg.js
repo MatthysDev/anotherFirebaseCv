d31c00caa3b905221ea68e4dbb5506a8aa52fd3b
Corentyn <corentyn.masset@gmail.com>
2021-11-22 15:35:06 +0100

    My first commit on this repo

---
+++ src/components/CompoImg.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class CompoImg extends Component{
+    render(){
+        return(
+            <>
+            
+            <img src="https://cdn.futura-sciences.com/buildsv6/images/largeoriginal/8/5/8/858743bb35_50169458_chien-min.jpg" alt="matthys" width="320px" style={{borderRadius:'50%', margin: 'auto'}}
+            />
+            </>
+        )
+    }
+}
+
+export default CompoImg;
---
+++ src/components/MEF.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import { Children } from 'react';
+
+class MEF extends Component{
+    render(){
+        return(
+            <>
+                
+            </>
+        )
+    }
+}
+
+export default MEF;
